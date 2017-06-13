e93939d9a7cee7592eca6c65d370fdf8b80d9d83
Vítor Miguel <vitomiguel@gmail.com>
2017-06-13 15:27:36 +0200

    Add SignUp VC Flow

---
+++ instaping/SignUpViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SignUpViewController.swift
+//  instaping
+//
+//  Created by Vítor Vazquez Miguel on 13/06/17.
+//  Copyright © 2017 BTS. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        if usernameText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: usernameText.text!, password: passwordText.text!, completion: { (user, error) in
+                if error != nil {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    
+                    UserDefaults.standard.set(user!.email, forKey: "userSigned")
+                    UserDefaults.standard.synchronize()
+                    
+                    let delegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+                    delegate.rememberLogin()
+                }
+            })
+            
+            
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Provide your E-mail and password.", preferredStyle: UIAlertControllerStyle.alert)
+            let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+
+    }
+
+}
