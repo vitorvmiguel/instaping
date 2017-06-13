8f9f95d878e58f0f7a55562e5f797cec1c7a8b7d
Vítor Miguel <vitomiguel@gmail.com>
2017-06-13 23:14:34 +0200

    Modify Favorites VC to Discover VC

--- instaping/DiscoverViewController.swift
+++ instaping/DiscoverViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FavoritesViewController: UIViewController {
+class DiscoverViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
